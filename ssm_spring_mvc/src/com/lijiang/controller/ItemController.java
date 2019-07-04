00d96edfa5b9c2eca9f39786c1613af7187dfce5
lijiang <2906718132@qq.com>
2019-07-04 23:28:52 +0800

    添加登录功能，并有拦截器和异常处理器。添加了分页插件bootstrap-table

--- ssm_spring_mvc/src/com/lijiang/controller/ItemController.java
+++ ssm_spring_mvc/src/com/lijiang/controller/ItemController.java
@@ -1,8 +1,8 @@
 package com.lijiang.controller;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,22 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
+ 
 import com.lijiang.bean.ItemInfo;
+import com.lijiang.exception.MyException;
+import com.lijiang.exception.MyLoginException;
 import com.lijiang.service.ItemService;
-
+ 
 @Controller
-@RequestMapping("/admin/items")
-public class ItemController {
-
+@RequestMapping("/admin/items/")
+public class ItemController extends BaseController {
 	@Autowired
 	private ItemService itemService;
 	
 //	显示所有列表
-	@RequestMapping("allList.do")
-	public String selectAll(Model model,ItemInfo itemInfo){
+	@RequestMapping("user/allList.do")
+	public String selectAll(Model model,ItemInfo itemInfo) throws MyException {
 		List<ItemInfo> itemList=itemService.selectAll(itemInfo);
-
+ 
 		model.addAttribute("itemList", itemList);
 		return "item_list";
 	}
@@ -34,21 +35,21 @@ public class ItemController {
 	@RequestMapping("delete.do")
 	public String delete(Integer id){
 		itemService.deleteById(id);
-		return "forward:allList.do";
+		return "forward:user/allList.do";
 	}
 	
 //	添加
 	@RequestMapping("save.do")
 	public String save(ItemInfo itemInfo){
 		itemService.saveItem(itemInfo);
-		return "forward:allList.do";
+		return "forward:user/allList.do";
 	}
 	
 //	修改
 	@RequestMapping("update.do")
     public String update(ItemInfo itemInfo){
     	itemService.updateItem(itemInfo);
-    	return "forward:allList.do";
+    	return "forward:user/allList.do";
     }
 	
 //	编辑 回显数据
@@ -57,5 +58,5 @@ public class ItemController {
 	public ItemInfo edit(Integer id) {
 		return itemService.selectItemInfoById(id);
 	}
-
+ 
 }
--- ssm_spring_mvc/src/com/lijiang/service/ItemServicelmpl.java
+++ ssm_spring_mvc/src/com/lijiang/service/ItemServicelmpl.java
@@ -1,20 +1,32 @@
 package com.lijiang.service;
-
+ 
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
 import com.lijiang.bean.ItemInfo;
+import com.lijiang.exception.MyException;
+import com.lijiang.exception.MyLoginException;
 import com.lijiang.mapper.ItemMapper;
-
+ 
 @Service
 public class ItemServicelmpl implements ItemService {
-
+ 
 	@Autowired
 	private ItemMapper itemMapper;
 	@Override
-	public List<ItemInfo> selectAll(ItemInfo itemInfo) {
+	public List<ItemInfo> selectAll(ItemInfo itemInfo) throws MyException{
+		
+//		运行时异常
+//	 int i=1/0;
+
+		
+//		自定义异常
+//		if(true){
+//			throw new MyException("列表没有获取到！！");
+//		}
+		
 		return itemMapper.selectAll(itemInfo);
 	}
 	@Override
@@ -32,11 +44,6 @@ public class ItemServicelmpl implements ItemService {
 	@Override
 	public ItemInfo selectItemInfoById(Integer id) {
 		return itemMapper.selectItemInfoById(id);
-	}
-
-	
-
-	
-	
-
+	}	
+ 
 }
